5668f7e32f9f012e47e5903926d52c6c9b95e1da
flipshare101 <nhovadia@gmail.com>
2022-12-09 11:18:27 -0700

    printing out obj file

--- python_code.py
+++ python_code.py
@@ -4,3 +4,12 @@ import pickle
 with open('python_object_files/all.obj', 'rb') as f:
   # Deserialize the object and store it in a variable
   my_object = pickle.load(f)
+  highest = 0
+  index = 0
+  for i in range(len(my_object)):
+      if(highest < my_object[i][1]):
+          print(i)
+          idnex = i
+          highest = my_object[i][1]
+      #print(my_object[i])
+  print(my_object[index])

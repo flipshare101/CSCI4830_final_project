ff5ec016b07eec48cefdb306a8eb86f7df894bcb
flipshare101 <nhovadia@gmail.com>
2022-12-10 18:05:48 -0700

    uploading code

--- python_code.py
+++ python_code.py
@@ -1,15 +1,26 @@
 import pickle
-
-# Open the file in binary read mode
+import matplotlib.pyplot as plt
+from sklearn.metrics import r2_score
+import csv
+import numpy as np
+x = []
+y = []
 with open('python_object_files/all.obj', 'rb') as f:
-  # Deserialize the object and store it in a variable
-  my_object = pickle.load(f)
-  highest = 0
-  index = 0
-  for i in range(len(my_object)):
-      if(highest < my_object[i][1]):
-          print(i)
-          idnex = i
-          highest = my_object[i][1]
-      #print(my_object[i])
-  print(my_object[index])
+# Deserialize the object and store it in a variable
+    my_object = pickle.load(f)
+    for i in range(len(my_object)):
+        x.append(my_object[i][0])
+        y.append(my_object[i][1])
+        #writer.writerow([my_object[i][2]])
+    ax = plt.subplots()
+    plt.scatter(x, y, color = 'b', s = 1)
+    #plt.xticks(rotation = 25)
+    z = np.polyfit(x, y, 1)
+    p = np.poly1d(z)
+    plt.plot(x, p(x), color = "r")
+    plt.xlabel('Iteration number')
+    plt.ylabel('Score')
+    plt.title('all optimizations', fontsize = 10)
+    r2 = r2_score(x, y)
+    plt.annotate('R2: ' + str(r2_score(y, p(x))), xy = (69, 900))
+    plt.show()
